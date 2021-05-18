c9fc197541abfdd762419c6aa62c95bdeef2e8fb
DevEnioOliveira <eniobackup.ti@hotmail.com>
2021-05-18 13:31:34 -0300

    conclusão do backend e inicio do front

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,14 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+const Container = (props) => {
+    return (
+        <div className="container" >
+            <h1>Teste</h1>
+        </div>
+    )
+}
+
+export default Container;
