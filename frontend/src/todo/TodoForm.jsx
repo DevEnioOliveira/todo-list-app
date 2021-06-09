5989cc3cc8c98c8477034925a41cd4541ff35f56
DevEnioOliveira <eniobackup.ti@hotmail.com>
2021-06-09 14:42:40 -0300

    placeholder modificado

--- frontend/src/todo/TodoForm.jsx
+++ frontend/src/todo/TodoForm.jsx
@@ -16,7 +16,7 @@ const TodoForm = (props) => {
             <div className="col-xs-12 col-sm-9 col-md-9 d-flex">
                 <input id="description" 
                         className="form-control" 
-                        placeholder="adicione uma tarefa" 
+                        placeholder="Adicione uma tarefa" 
                         value={props.description}
                         onKeyUp={keyHandler}
                         onChange={props.handleChange}
